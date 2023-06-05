40412109bf1803d2fd9355d6d6b2d4a62744936c
Natalia <113330261+nalmarazgutierrez@users.noreply.github.com>
2023-06-05 23:21:18 +0200

    optimizing code

--- GROMeal/client/src/views/ShoppingListView.js
+++ GROMeal/client/src/views/ShoppingListView.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { NavLink, useParams, Route, Routes, useNavigate } from 'react-router-dom';
 import SpoonApi from "../helpers/SpoonApi";
-
+import RecipesContext from "../components/RecipesContext";
+import { useContext } from "react";
 import ProgressBar from '../components/ProgressBar';
 
 
@@ -12,9 +13,14 @@ function ShoppingListView() {
 
     const [recipes, setRecipes] = useState([]);
     const [planRecipes, setPlanRecipes] = useState([]);
+    
     const [ingredients, setIngredients] = useState([]);
+    const [items, setItems] = useState([]);
+
     // const [pdf, setPdf] = useState(null);
     const { planId } = useParams();
+    const { addedItems, setAddedItems} = useContext(RecipesContext);
+
   
     useEffect(() => {
         getRandomRecipes();
@@ -36,14 +42,18 @@ function ShoppingListView() {
       getPlanRecipes();
     }, []);
 
-    // useEffect(() => {
-    //     getIngredients();
-    //   }, [planRecipes]);
-
     
+
+   
 
   //Get all recipes of the json
   //Find the API_id for each planRecipe
+      //FUNCTION TO CLICK ON RECIPE, VISUALIZE RECIPE ON TOP & ADDS RECIPE'S DATA TO CONST addedRecipe
+    //   function showIngredients(id){
+    //     let ingredient = recipes.find(r => r.id === id);
+    //     setFeatRecipe(selectedRecipe);
+    //     setAddedRecipe((addedRecipe) => ({...addedRecipe, API_id: selectedRecipe.id, recipe_title: selectedRecipe.title, recipe_image: selectedRecipe.image}));
+    // };
   
   // Get All recipes from a plan
   async function getPlanRecipes() {
@@ -60,25 +70,43 @@ function ShoppingListView() {
       console.log(`Server error: ${err.message}`);
   }
   }
-console.log(planRecipes);
+  // console.log(recipes)
+  // console.log(planRecipes)
+  // console.log(planRecipes[0].API_id)
+  // console.log(planRecipes[0].servings)
+  // console.log(planRecipes.length)
+
   //planRecipes es un array de objetos
 
+  // let recipeId =  planRecipes.map(recipe => recipe.API_id)
+  // console.log(recipeId)
+  // let recipesIngredients = [];
+  // for(let i=0; i<=recipeId.length; i++){
+  //       let foundRecipe = recipes.find(r => r.id === recipeId);
+  //       let recipeIngredient = foundRecipe.extendedIngredients
+  //       recipeIngredient =  recipeIngredient.map(ingredient => ({...recipeIngredient, name: ingredient.name}))
+  //       recipesIngredients+=recipeIngredient;
+  //     }
+  // console.log(recipesIngredients)
+
+  // get ingredients by the id of each recipe
+  
+  // getIngredients()
+  // console.log(recipesIngredients);
 
 // get original servings of each plan recipe
-
-// function findServings(recipeName) {
-//   const recipe = recipes[recipeName];
-//   let totalServings = recipe.servings;
-//   for (const ingredient in recipe.ingredients) {
-//     const amount = recipe.ingredients[ingredient].amount;
-//     const servingSize = recipe.ingredients[ingredient].servingSize;
-//     totalServings *= amount / servingSize;
-//   }
-//   return totalServings;
+  
+//   function findServings(recipeName) {
+//     const recipe = recipes[recipeName];
+//     let totalServings = recipe.servings;
+//     for (const ingredient in recipe.ingredients) {
+//       const amount = recipe.ingredients[ingredient].amount;
+//       const servingSize = recipe.ingredients[ingredient].servingSize;
+//       totalServings *= amount / servingSize;
+//     }
+//     console.log(totalServings);
+//     return totalServings;
 // }
-
-// 
-
 
 // get ingredients by the id of each recipe
   // const getIngredients = async () => {
@@ -94,106 +122,34 @@ console.log(planRecipes);
   // };
 // ingredients también es un array de objetos 
 // Cada objeto se llama ingredients y es otro array de objetos
-console.log(ingredients);
+// console.log(recipes);
 // console.log(ingredients);
-// const shoppingList = []
-    
-//             for (let i = 0; i < ingredients.length; i++) {
-//                 let array = ingredients[i].ingredients;
-//                 // console.log(ingredients);
-//                 for (let j = 0; j < array.length; j++) {
-//                     console.log(array);
-//                     let newObject = {}
-//                     // newObject.id = 0;
-//                     console.log(newObject.id);
-//                     newObject.item_name = array[j].name;
-//                     newObject.amount = array[j].amount.metric.value;
-//                     newObject.unit = array[j].amount.metric.unit;
-//                     console.log(newObject);
-//                     shoppingList.push(newObject);    
-//                 }
-//             }
-//             console.log(shoppingList);
 
+const shoppingList = [] 
+    //FETCH POST NEW RECIPE FROM USER
+    // const addRecipe = async () => {
 
-
-        const shoppingList = [{id: 1, item_name: 'canned corn', amount: 425.243, unit: 'g'},
-        {id: 2, item_name: 'canned black beans', amount: 425.243, unit: 'g'},
-        {id: 3, item_name: 'sweet diced canned tomatoes', amount: 411.068, unit: 'g'}, 
-        {id: 4, item_name: 'canned swanson premium chicken', amount: 170.097, unit: 'g'},
-        {id: 5, item_name: 'canned enchilada sauce', amount: 283.495, unit: 'g'},
-        {id: 6, item_name: "canned campbell's cream of mushroom soup", amount: 304.757, unit: 'g'},
-        {id: 7, item_name: 'milk', amount: 366, unit: 'ml'},
-        {id: 8, item_name: 'tortilla chips', amount: 4, unit: 'servings'}, 
-        {id: 9, item_name: 'shredded cheese', amount: 4, unit: 'servings'}, 
-        {id: 10, item_name: 'canned corn', amount: 425.243, unit: 'g'}, 
-        {id: 11, item_name: 'canned black beans', amount: 425.243, unit: 'g'}, 
-        {id: 12, item_name: 'sweet diced canned tomatoes', amount: 411.068, unit: 'g'}, 
-        {id: 13, item_name: 'canned swanson premium chicken', amount: 170.097, unit: 'g'}, 
-        {id: 14, item_name: 'canned enchilada sauce', amount: 283.495, unit: 'g'}, 
-        {id: 15, item_name: "canned campbell's cream of mushroom soup", amount: 304.757, unit: 'g'}, 
-        {id: 16, item_name: 'milk', amount: 366, unit: 'ml'}, 
-        {id: 17, item_name: 'tortilla chips', amount: 4, unit: 'servings'}, 
-        {id: 18, item_name: 'shredded cheese', amount: 4, unit: 'servings'}, 
-        {id: 19, item_name: 'canned cherry pie filling', amount: 236.588, unit: 'ml'}, 
-        {id: 20, item_name: 'cocoa powder', amount: 3, unit: 'Tbsps'}, 
-        {id: 21, item_name: 'cream cheese', amount: 1.5, unit: 'pkg'}, 
-        {id: 22, item_name: 'eggs', amount: 2, unit: ''}, 
-        {id: 23, item_name: 'gelatin', amount: 0.571, unit: 'tsps'}, 
-        {id: 24, item_name: 'heavy cream', amount: 238, unit: 'ml'}, 
-        {id: 25, item_name: 'oreo cookies', amount: 12, unit: ''}, 
-        {id: 26, item_name: 'powdered sugar', amount: 2, unit: 'Tbsps'}, 
-        {id: 27, item_name: 'salt', amount: 1, unit: 'tsp'}, 
-        {id: 28, item_name: 'semi-sweet chocolate chips', amount: 131.25, unit: 'ml'}, 
-        {id: 29, item_name: 'sour cream', amount: 57.5, unit: 'ml'}]
-        // {id: 30, item_name: 'sugar', amount: 133.333, unit: 'g'}, 
-        // {id: 31, item_name: 'unsalted butter', amount: 1.521, unit: 'Tbsps'}, 
-        // {id: 32, item_name: 'vanilla extract', amount: 0.323, unit: 'tsps'}, 
-        // {id: 33, item_name: 'water', amount: 0.75, unit: 'cups'}, 
-        // {id: 34, item_name: 'canned cherry pie filling', amount: 236.588, unit: 'ml'}, 
-        // {id: 35, item_name: 'cocoa powder', amount: 3, unit: 'Tbsps'}, 
-        // {id: 36, item_name: 'cream cheese', amount: 1.5, unit: 'pkg'}, 
-        // {id: 37, item_name: 'eggs', amount: 2, unit: ''}, 
-        // {id: 38, item_name: 'gelatin', amount: 0.571, unit: 'tsps'}, 
-        // {id: 39, item_name: 'heavy cream', amount: 238, unit: 'ml'}, 
-        // {id: 40, item_name: 'oreo cookies', amount: 12, unit: ''}, 
-        // {id: 41, item_name: 'powdered sugar', amount: 2, unit: 'Tbsps'}, 
-        // {id: 42, item_name: 'salt', amount: 1, unit: 'tsp'}, 
-        // {id: 43, item_name: 'semi-sweet chocolate chips', amount: 131.25, unit: 'ml'}, 
-        // {id: 44, item_name: 'sour cream', amount: 57.5, unit: 'ml'}, 
-        // {id: 45, item_name: 'sugar', amount: 133.333, unit: 'g'}, 
-        // {id: 46, item_name: 'unsalted butter', amount: 1.521, unit: 'Tbsps'}, 
-        // {id: 47, item_name: 'vanilla extract', amount: 0.323, unit: 'tsps'}, 
-        // {id: 48, item_name: 'water', amount: 0.75, unit: 'cups'}]
-
-        //NEWLIST sum amounts when names match
-        
-        let newList = Object.values(shoppingList.reduce((value, object) => {
-            if (value[object.item_name]) {
-              value[object.item_name].amount += object.amount; 
-              value[object.item_name].count++;
+    //   try {
+    //       let response = await Api._doFetch(`/api/recipes/${planId}`, 'POST', addedRecipe);
+    //       console.log(response);
+    //       if (response.ok) {  
+    //           console.log('Recipe added!')
+    //       } else {
+    //           console.log(`Server error: ${response.status}:
+    //           ${response.statusText}`);
+    //       }
           
-          } else {
-              value[object.item_name] = { ...object , count : 1
-              };
-            }
-            return value;
-          }, {}));
-          
-           //add an id to every item in the list
-        //   for (let index = 0; index < newList.length; index++) {
-        //     newList[index].id = index + 1;
-        //   }
-        //If units is equal to "" or "servings", then replace with
-
-        
-        
+    //   } catch (err) {
+    //       console.log(`Network error: ${err.message}`);
+    //   }
+    //   console.log(addedRecipe)
+    // };     
     
     //POST SHOPPING ITEMS TO THE LIST (when creating the plan¿?)
-    // const addItem = async (newList) => {
+    // const addItem = async () => {
     
     //   try {
-    //       let response = await Api._doFetch(`/api/recipes/${planId}`, 'POST', newList);
+    //       let response = await Api._doFetch(`/api/list/${planId}`, 'POST', newList);
     //       console.log(response);
     //       if (response.ok) {            
     //           console.log('Recipe added!')
@@ -205,46 +161,93 @@ console.log(ingredients);
     //   } catch (err) {
     //       console.log(`Network error: ${err.message}`);
     //   }
-    
+    //   console.log(newList)
     // };
+    //   // // Add every item (POST)
+    //   for (let i = 0; i < newList.length; i++) {
+    //     console.log("Hello")
+    //   //   addItem(addedItems);
+    //     setAddedItems((addedItems) => ({...addedItems, id: newList[i].id, item_name: `${newList[i].item_name}`, amount: newList[i].amount, unit: `${newList[i].unit}`}));
+    //   }
+    //   console.log(addedItems);
 
-    // Add every item (POST)
-    // for (let i = 0; i < newList.length; i++) {
-    //   addItem(addedItem);
-    //   setAddedItem((addedItem) => ({...addedItem, id: newList[i].id, item_name: `${newList[i].item_name}`, amount: newList[i].amount, unit: `${newList[i].unit}`}));
-    // }
-    // console.log(addedItem);
 
 
-    //DELETE AN ITEM
-// async function deleteItem( id) {
-    // Define fetch() options
-//     let options = {
-//         method: 'DELETE'
-//     };
+    //POST function to modify a recipe
+  //   async function addItem() {
+  //     let options = {
+  //         method: 'POST',
+  //         headers: { 'Content-Type': 'application/json' },
+  //         body: JSON.stringify(newList)
+  //     };
+
+  //     try {
+  //         let response = await fetch(`/api/list/${planId}`, options);
+  //         if (response.ok) {
+  //             let newList = await response.json();
+  //             setAddedItems(newList);
+  //         } else {
+  //             console.log(`Server error: ${response.status} ${response.statusText}`);
+  //         }
+  //     } catch (err) {
+  //         console.log(`Server error: ${err.message}`);
+  //     }
+  // }
+
   
-//     try {
-//         let response = await fetch(`/api/list/${planId}/${id}`, options);
-//         if (response.ok) {
-//             let items = await response.json();
-//             setAddedItem(items);
-//         } else {
-//             console.log(`Server error: ${response.status} ${response.statusText}`);
-//         }
-//     } catch (err) {
-//         console.log(`Server error: ${err.message}`);
-//     }
-//   }
+
+  // useEffect(() => {
+  //   addItem();
+  // }, []);
+
+  useEffect(() => {
+    getItems();
+  }, []);
+
+  async function getItems() {
+
+    try {
+      let response = await fetch(`/api/list/${planId}`);
+      if (response.ok) {
+          let newList = await response.json();
+          setAddedItems(newList);
+      } else {
+          console.log(`Server error: ${response.status} ${response.statusText}`);
+      }
+  } catch (err) {
+      console.log(`Server error: ${err.message}`);
+  }
+  }
+
+
           
+  async function deleteIngredient(item_name) {
+    // Define fetch() options
+    let options = {
+        method: 'DELETE'
+    };
+  
+    try {
+        let response = await fetch(`/api/list/${planId}/${item_name}`, options);
+        if (response.ok) {
+            let exercises = await response.json();
+            setAddedItems(exercises);
+        } else {
+            console.log(`Server error: ${response.status} ${response.statusText}`);
+        }
+    } catch (err) {
+        console.log(`Server error: ${err.message}`);
+    }
+  }
     // DOWNLOAD FUNCTION
   let weekDayArray = ['monday', 'tuesday', 'wednesday', 'thursday', "friday", "saturday", "sunday"];
 
-  const downloadPdf = event =>{
+  const downloadPdf = event => {
     var doc = new jsPDF({
       // unit:"mm"
     });
-    shoppingList.forEach(function(ingredient,i){
-      doc.text(50,10+i*10, ingredient.item_name + " " + ingredient.amount + " " + ingredient.unit +"\n" );
+    shoppingList.forEach(function(newList,i){
+      doc.text(50,10+i*10, newList.item_name + " " + newList.amount + " " + newList.unit +"\n" );
     });
     doc.setFontSize(5);
 
@@ -278,9 +281,37 @@ console.log(ingredients);
           <h1 className="col" id="title">My Shopping List</h1>
             <button id="buttonA" className="btn btn-warning btn-md col-4" onClick={downloadPdf}>DOWNLOAD</button>
             </div>
-            
+
             {
-            newList.map(item => (
+            // recipesIngredients.map(item => (
+            //     <div className="card" key={item.id}>
+            //         <div className="row p-2">
+            //              {/* <div className='col-1' >
+                            
+            //                 {item.id}
+            //             </div> */}
+            //             <div className='col-6 px-5'>
+                            
+            //                 {item.item_name}
+            //             </div>
+            //             <div className='col-3'>
+            //                 {Math.round(item.amount)}
+            //             </div>
+                    
+            //             <div className='col-1'>
+                            
+            //                 {item.unit}
+            //             </div>
+            //             <div className="col-1 content-right">
+            //               <button id="buttonA" className="btn btn-warning btn-sm" title="delete" type="button" onClick = {deleteIngredient}>x</button>
+            //             </div>
+            //         </div>
+            //     </div>
+            // ))
+          }
+            
+          {
+            addedItems.map(item => (
                 <div className="card" key={item.id}>
                     <div className="row p-2">
                          {/* <div className='col-1' >
@@ -300,12 +331,12 @@ console.log(ingredients);
                             {item.unit}
                         </div>
                         <div className="col-1 content-right">
-                          <button id="buttonA" className="btn btn-warning btn-sm" title="delete" type="button">x</button>
+                          <button id="buttonA" name={item.item_name} className="btn btn-warning btn-sm" title="delete" type="button" onClick = {(e) => deleteIngredient(item.item_name)} >x</button>
                         </div>
                     </div>
                 </div>
             ))
-        }
+          }
         </div>
     </div>
       </div>
